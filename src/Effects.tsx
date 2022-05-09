6fffcdc71c8b6b1c0e34173aff86030a272963bd
fateful1 <facelessasha@yandex.ru>
2022-05-10 01:48:23 +0500

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+    const callback = (x: number) => {
+        setMessage(x);
+    };
+
+    useEffect(() => {
+        subscribe(props.sourceId, callback);
+        setMessage(-1);
+        return () => {
+            unsubscribe(props.sourceId, callback);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
